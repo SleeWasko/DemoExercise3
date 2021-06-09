803b3112c88a2a6427cc4156b8767197c9aef2ff
Savannah <swasko123@>
2021-06-09 14:40:46 -0600

    Add project files.

---
+++ DemoExercise3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DemoExercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Linus Torvalds");
+            names.Add("Donald Knuth");
+            names.Add("Grace Hopper");
+            names.Add("Donald Knuth");
+            names.Add("Jean Bartik");
+
+            names.TrimExcess();
+            names.RemoveAt(3);
+            names.Remove("Donald Knuth");
+
+            DisplayNames(names);
+
+
+            Console.WriteLine("Count of list: " + names.Count);
+            Console.WriteLine("Capacity of list: " + names.Capacity);
+
+
+        }
+
+        private static void DisplayNames(List<string> names)
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine((i + 1) + "; " + names[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
